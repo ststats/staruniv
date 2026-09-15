e50c9dcefeaf41d090a1a09e5f364e9b6725c007
ststats <252nds@gmail.com>
2026-09-15 14:53:16 +0900

    Delete templates directory

--- templates/assets/page-records.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const filterItems = document.querySelectorAll('#crewFilterList .filter-item');
-  filterItems.forEach(item => {
-    item.addEventListener('click', () => {
-      filterItems.forEach(i => i.classList.remove('is-active'));
-      item.classList.add('is-active');
-    });
-  });
-});
